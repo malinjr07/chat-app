733774ad5a85d68bccbfae39ab7a045a21443c36
malinjr07 <malinjr07@gmail.com>
2024-09-27 20:12:18 +0600

    firebase init

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const PrivateRoutes: FC = () => {
-  let auth = { token: true };
+  let auth = { token: false };
   return auth.token ? <Outlet /> : <Navigate to='/login' />;
 };
 
